8ca89ffb7045b38c209090b30dc86edec4649de4
AisunMagaramova <maharramovaaysun1122@gmail.com>
2025-11-28 14:15:51 +0000

    t3

---
+++ python_variable_annotations/3-to_str.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+""" arqumenti n qebuil eden float"""
+
+
+def to_str(n: float) -> str:
+"""float-ınsetir temsilçiliyini qaytaran to_str tipli annotasiyalı fun"""
+    return str(n)
