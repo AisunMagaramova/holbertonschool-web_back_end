fe326c914336fb1ca094ae4e06ce1844123b990c
AisunMagaramova <maharramovaaysun1122@gmail.com>
2025-11-28 14:48:32 +0000

    t1

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -4,13 +4,23 @@ import asyncio
 from typing import List
 import random
 
-
+# import edirik
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
 async def wait_n(n: int, max_delay: int) -> List[float]:
-    """ Function that returns the list
     """
-    tasks  = [asyncio.create_task(wait_random(max_delay)) for _ in range(n)]
+    Function that returns the list of results from n coroutines
+
+    Args:
+        n (int): Number of coroutines to run
+        max_delay (int): Maximum delay for wait_random
+
+    Returns:
+        List[float]: List of returned float values
+    """
+    # create tasks
+    l = [asyncio.create_task(wait_random(max_delay)) for _ in range(n)]
+    # await their completion as they finish
     finish = [await task for task in asyncio.as_completed(l)]
-    return results
+    return finish
