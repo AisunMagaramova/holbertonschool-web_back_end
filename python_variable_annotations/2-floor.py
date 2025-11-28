c7c087ac8d222ae95f498b9c204546cfb765c60d
AisunMagaramova <maharramovaaysun1122@gmail.com>
2025-11-28 13:57:50 +0000

    t2

---
+++ python_variable_annotations/2-floor.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+import math
+""" arqumet olaraq float n qebul edilir"""
+
+
+def floor_floar (n: float) -> int:
+""" floatn pol deyerini qaytarir"""
+    return floor_floar(n)
