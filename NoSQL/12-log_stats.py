9b49a2714c169b9101f9527416cf957e4ec01e96
AisunMagaramova <maharramovaaysun1122@gmail.com>
2025-12-21 17:30:37 +0000

    t12

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -3,29 +3,27 @@
 """
 from pymongo import MongoClient
 
-
 METHODS = ["GET", "POST", "PUT", "PATCH", "DELETE"]
 
 
 def log_stats(mongo_collection, option=None):
-    """ script that provides some stats about Nginx logs stored in MongoDB
-    """
-    items = {}
+    """Provides stats about Nginx logs stored in MongoDB"""
     if option:
-        value = mongo_collection.count_documents(
-            {"method": {"$regex": option}})
+        value = mongo_collection.count_documents({"method": option})
         print(f"\tmethod {option}: {value}")
         return
 
-    result = mongo_collection.count_documents(items)
-    print(f"{result} logs")
+    total = mongo_collection.count_documents({})
+    print(f"{total} logs")
     print("Methods:")
     for method in METHODS:
-        log_stats(nginx_collection, method)
+        log_stats(mongo_collection, method)
+
     status_check = mongo_collection.count_documents({"path": "/status"})
     print(f"{status_check} status check")
 
 
 if __name__ == "__main__":
-    nginx_collection = MongoClient('mongodb://127.0.0.1:27017').logs.nginx
+    client = MongoClient("mongodb://127.0.0.1:27017")
+    nginx_collection = client.logs.nginx
     log_stats(nginx_collection)
