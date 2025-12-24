7e696fc495edb136e8934e317592bd0249589512
AisunMagaramova <maharramovaaysun1122@gmail.com>
2025-12-24 12:27:34 +0000

    t0

---
+++ ES6_basic/0-constants.js
@@ -0,0 +1,16 @@
+// taskFirst funksiyasi yaradilir
+function taskFirst (){
+	// const istifade edirik cunki sonradan deyeismeyecek
+	const task = 'I prefer const when I can.';
+	// task deyisen deyeri qaytarir
+	return task;
+}
+// taskNext adli funksiya yatrdilir
+function taskNext (){
+	//combinationadli adli deyisen yaradilir
+	//left istifade edirik cunki sonra deyeri deyisdireceyik
+	left combination = 'But sometimes let';
+	// combination deyisenini deyeri yeniden deyisdiririk 
+	combination = "Let me change it";
+	return combination;
+}
