ebb6367c533c157818ab770a5fc0e84259f16dda
AisunMagaramova <maharramovaaysun1122@gmail.com>
2026-01-25 23:08:30 +0400

    Add full server structure

--- Node_JS_basic/full_server/utils.js
+++ Node_JS_basic/full_server/utils.js
@@ -1,32 +1,34 @@
-import fs from 'fs';
+const fs = require('fs');
 
-const readDatabase = (path) => new Promise((resolve, reject) => {
-  fs.readFile(path, 'utf8', (err, data) => {
-    if (err) {
-      reject(err);
-      return;
-    }
-
-    const lines = data.trim().split('\n');
-    const students = lines.slice(1);
-
-    const fields = {};
-
-    students.forEach((line) => {
-      if (line) {
-        const parts = line.split(',');
-        const firstname = parts[0];
-        const field = parts[parts.length - 1];
-
-        if (!fields[field]) {
-          fields[field] = [];
-        }
-        fields[field].push(firstname);
+function readDatabase(path) {
+  return new Promise((resolve, reject) => {
+    fs.readFile(path, 'utf8', (err, data) => {
+      if (err) {
+        reject(Error(err));
+        return;
       }
-    });
+      const content = data.toString().split('\n');
 
-    resolve(fields);
+      let students = content.filter((item) => item);
+
+      students = students.map((item) => item.split(','));
+
+      const fields = {};
+      for (const i in students) {
+        if (i !== 0) {
+          if (!fields[students[i][3]]) fields[students[i][3]] = [];
+
+          fields[students[i][3]].push(students[i][0]);
+        }
+      }
+
+      delete fields.field;
+
+      resolve(fields);
+
+      //   return fields;
+    });
   });
-});
+}
 
 export default readDatabase;
